f1bf4444bae9136ae7340961b09f174a141f3139
Robert Hargreaves <kiyouta@gmail.com>
2025-11-24 22:26:05 +0000

    feat: use pytest for tests

--- test/etcd-test.py
+++ test/etcd-test.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
+import warnings
+
+warnings.filterwarnings("ignore", category=DeprecationWarning, module="google.protobuf")
+warnings.filterwarnings("ignore", category=DeprecationWarning, module="etcd3")
 
 import etcd3
 
-etcd = etcd3.client(host='127.0.0.1', port=2379)
 
-etcd.put('/test/key1', 'hey key1')
-etcd.put('/test/key2', 'hey key2')
+def test_etcd():
+    etcd = etcd3.client(host="127.0.0.1", port=2379)
 
-keys = etcd.get_prefix('/test')
+    etcd.put("/test/key1", "hey key1")
+    etcd.put("/test/key2", "hey key2")
 
-for key in keys:
-    print(key[0].decode("utf-8"))
+    keys = list(etcd.get_prefix("/test"))
+
+    assert keys[0][0].decode("utf-8") == "hey key1"
+    assert keys[1][0].decode("utf-8") == "hey key2"
